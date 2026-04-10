795bf161d60c3517d25bdad539833ae44d9a9ee1
rh55123 <reneehorvat55@gmail.com>
2026-04-10 09:26:30 +0200

    Add files via upload

---
+++ vjezba1/vjezba1.php
@@ -0,0 +1,21 @@
+<?php
+$naslov = "Moj prvi PHP dokument";
+$autor = "Renee Horvat";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradila je <strong>$autor</strong>.</p>";
+echo '<a href="https://www.index.hr" target="_blank">Posjeti INDEX.HR</a>';
+?>
+
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $naslov; ?></title>
+</head>
+<body>
+
+</body>
+</html>
+
+<!-- vjezba1.php -->
